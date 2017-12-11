7c8b4c9063f81840c4cc737ef8f23c1c2e47d74d
Bruno Klein <bruno.klein@digitaldesk.com.br>
2017-12-11 15:20:53 -0200

    Forms layout finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import	React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyBjNpC8sZgN-z-TxcI9EL9yOBBZdeu8IPA',
+        authDomain: 'authentication-c8c3b.firebaseapp.com',
+        databaseURL: 'https://authentication-c8c3b.firebaseio.com',
+        projectId: 'authentication-c8c3b',
+        storageBucket: 'authentication-c8c3b.appspot.com',
+        messagingSenderId: '795791495317'
+      });
+  }
+
   render(){
     return(
       <View>
-      <Text>An app</Text>
+      <Header headerText="Authentication" />
+      <LoginForm />
       </View>
     );
   }
